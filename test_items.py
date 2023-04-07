e3ac26c33674bbfc2f970e657fba9550f3a4170a
Stasya2004 <129410429+Stasya2004@users.noreply.github.com>
2023-04-07 14:58:30 +0300

    adding_code

--- test_items.py
+++ test_items.py
@@ -1,1 +1,11 @@
+from selenium.webdriver.common.by import By
+import time
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+class TestProductPage:
+    def test_button_add_to_basket_exist_on_page(self, browser):
+        browser.get(link)
+        time.sleep(5)
+        button = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form > button')
+        assert button, 'button not found'
